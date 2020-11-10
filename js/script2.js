3d07fc90a71825607fcc733c34a5af86baefaa8f
ChristianPucci91 <pucci.christian91@gmail.com>
2020-11-10 17:09:42 +0100

    ESERCIZIO 2 UPDATE stampa in HTML

--- js/script2.js
+++ js/script2.js
@@ -18,10 +18,12 @@ var arrayObj = [
   {name: 'Jenny', type: 'borsa', color: 'pink'},
 ];
 
+var stampa = ""; // stampo in HTML
 for (var key in arrayObj) { // stampo l'array originale senza nuova proprietà
   console.log("array iniziale ",arrayObj[key]);
+  stampa += arrayObj[key].name + " , " + arrayObj[key].type + " , " + arrayObj[key].color + "<br>";
 }
-
+document.getElementById('array_base').innerHTML = stampa;
 // Array copia vuoto
 var arrayObjCopia = [];
 var x = 0;
@@ -32,19 +34,23 @@ for (var i = 0; i < arrayObj.length; i++) {
   x++;
 } // fine ciclo for
 
+var stampa2 = ""; // stampo in HTML
 for (var key in arrayObjCopia) { // stampo l'array copia senza nuova proprietà
  console.log("array copia",arrayObjCopia[key]);
+ stampa2 += arrayObjCopia[key].name + " , " + arrayObjCopia[key].type + " , " + arrayObjCopia[key].color + "<br>";
 }
+document.getElementById('array_copia').innerHTML = stampa2;
 
 // creo array di lettere dell'alfabeto per fare uscire in output una lettera casuale
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
-
+var stampa3 = "";
 // ciclo for dove dico in ogni oggetto dell'array creami una proprietà position che sarà uguale a una lettera casuale
 // data dal valore della variabile "min"
 for (var variable in arrayObjCopia) {
   min = alphabet[Math.floor(Math.random()*alphabet.length)];
   arrayObjCopia[variable].position = min;
   console.log("array nuova proprietà",arrayObjCopia[variable]);
+  stampa3 += arrayObjCopia[variable].name + " , " + arrayObjCopia[variable].type + " , " + arrayObjCopia[variable].color + " , " + arrayObjCopia[variable].position + "<br>";
 }
-
+document.getElementById('stampa_obj').innerHTML = stampa3;
 //----------------------- FINE JSNACK 2 ---------------------
